6a7f3e7017e03765cf2d303e4df0c16dc8b80fbc
cyberkingz <mends.mooch.0t@icloud.com>
2025-01-02 02:38:41 +0100

    update colleciton view 2 products on mobile

--- app/routes/($locale).collections.all.jsx
+++ app/routes/($locale).collections.all.jsx
@@ -73,7 +73,7 @@ export default function Collection() {
         {products.nodes.length > 0 ? (
           <PaginatedResourceSection
             connection={products}
-            resourcesClassName="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8"
+            resourcesClassName="products-grid"
           >
             {({node: product, index}) => (
               <ProductItem
